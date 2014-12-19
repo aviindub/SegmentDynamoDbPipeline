d318023a1ddb12fb0d7c5ad6aa975efc1f9efb4d
aviindub <aviindub@gmail.com>
2014-12-19 13:13:13 -0800

    use the aws client directly instead of dynamodb-doc.

--- segmentDynamoDbPipeline.js
+++ segmentDynamoDbPipeline.js
@@ -1,39 +1,52 @@
 var iron_mq = require('iron_mq');
+var worker = require('node_helper');
 var AWS = require("aws-sdk");
-var DOC = require("dynamodb-doc");
+//var DOC = require("dynamodb-doc");
+var Guid = require("guid");
 
 var imq = new iron_mq.Client();
 var queue = imq.queue("my_queue");
-var docClient = new DOC.DynamoDB();
+//var docClient = new DOC.DynamoDB();
 
 AWS.config.update({
     accessKeyId: worker.config.AWS_ACCESS_KEY_ID,
     secretAccessKey: worker.config.AWS_SECRET_ACCESS_KEY,
     region: worker.config.AWS_REGION
 });
-
-var worker = require('node_helper');
+var dd = new AWS.DynamoDB();
 
 console.log("params:", worker.params);
 //console.log("config:", worker.config);
 console.log("task_id:", worker.task_id);
 
+var messageToItem = function(message) {
+    message_parsed = JSON.parse(message.body);
+    var item = {
+        "os_id": {"S": Guid.raw()},
+        "timestamp": {"N": message_parsed.timestamp},
+        "message": {"S": message.body}
+    };
+    return item;
+}
 
 var options = {n: 100};
 queue.get_n(options, function(error, messages) {
     messages.forEach(function(message) {
-        // do we need to insert a GUID or something on the message?
-        docClient.putItem(JSON.parse(message.body), function(err, data) {
+        params = {
+            "TableName": worker.config.SEGMENT_RAW_DATA_TABLE_NAME,
+            "Item": messageToItem(message)
+        }
+        dd.putItem(params, function(err, data) {
             if (err) {
-                //log error
+                console.log(err);
             }
             else {
                 message.delete();
             }
             if (data) {
                 //what is this?
+                console.log("got data back from putItem:", data.toString());
             }
         });
     });
 });
-
