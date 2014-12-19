1ef8004e918edf3579042781300dba5c32280016
aviindub <aviindub@gmail.com>
2014-12-19 15:42:38 -0800

    version works with dynamoDB, still needs to be adapted for use with the segment queue.

--- segmentDynamoDbPipeline.js
+++ segmentDynamoDbPipeline.js
@@ -1,12 +1,10 @@
 var iron_mq = require('iron_mq');
 var worker = require('node_helper');
 var AWS = require("aws-sdk");
-//var DOC = require("dynamodb-doc");
 var Guid = require("guid");
 
 var imq = new iron_mq.Client();
 var queue = imq.queue("my_queue");
-//var docClient = new DOC.DynamoDB();
 
 AWS.config.update({
     accessKeyId: worker.config.AWS_ACCESS_KEY_ID,
@@ -15,19 +13,20 @@ AWS.config.update({
 });
 var dd = new AWS.DynamoDB();
 
-console.log("params:", worker.params);
-//console.log("config:", worker.config);
-console.log("task_id:", worker.task_id);
+// console.log("params:", worker.params);
+// console.log("config:", worker.config);
+// console.log("task_id:", worker.task_id);
 
 var messageToItem = function(message) {
-    message_parsed = JSON.parse(message.body);
+    // message_parsed = JSON.parse(message);
+    // console.log("type of params part deux:", typeof message);
     var item = {
         "os_id": {"S": Guid.raw()},
-        "timestamp": {"N": message_parsed.timestamp},
-        "message": {"S": message.body}
+        "timestamp": {"N": new Date(message.timestamp).valueOf().toString()},
+        "message": {"S": JSON.stringify(message)}
     };
     return item;
-}
+};
 
 var dynamoDbPutMessage = function(message) {
     var params = {
@@ -35,28 +34,37 @@ var dynamoDbPutMessage = function(message) {
         "Item": messageToItem(message)
     }
     dd.putItem(params, function(err, data) {
-        if (err) {
-            console.log(err);
-        }
-        else {
-            message.delete();
-        }
         if (data) {
             //what is this?
-            console.log("got data back from putItem:", data.toString());
+            console.log("got data back from putItem:", data);
+        }
+        if (err) {
+            console.log(err);
+            // process.exit();
+        }
+        else {
+            console.log("no error. exiting.");
+            message.delete();
+            // process.exit();
         }
     });
-}
+};
 
 //debug
 if (worker.params) {
+    //console.log("type of params:", typeof worker.params);
     dynamoDbPutMessage(worker.params);
 }
 //end debug
 
 var options = {n: 100};
 queue.get_n(options, function(error, messages) {
-    messages.forEach(function(message) {
-        dynamoDbPutMessage(message);
-    });
+    if (error) {
+        console.log("error getting messages from queue:", error);
+    }
+    if (messages) {
+        messages.forEach(function(message) {
+            dynamoDbPutMessage(message);
+        });
+    }
 });
