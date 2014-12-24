2a05439227e30a64b9656970a2c17905b5f99244
aviindub <aviindub@gmail.com>
2014-12-24 14:14:03 -0800

    mostly working version. needs to be refactored so that it can delete messages from the queue.

--- segmentDynamoDbPipeline.js
+++ segmentDynamoDbPipeline.js
@@ -4,7 +4,7 @@ var AWS = require("aws-sdk");
 var Guid = require("guid");
 
 var imq = new iron_mq.Client();
-var queue = imq.queue("my_queue");
+var queue = imq.queue(worker.config.IRON_MQ_QUEUE_NAME); // "segment"
 
 AWS.config.update({
     accessKeyId: worker.config.AWS_ACCESS_KEY_ID,
@@ -18,12 +18,15 @@ var dd = new AWS.DynamoDB();
 // console.log("task_id:", worker.task_id);
 
 var messageToItem = function(message) {
-    // message_parsed = JSON.parse(message);
+    message_parsed = JSON.parse(message);
     // console.log("type of params part deux:", typeof message);
+    // console.log("message parsed:", typeof message_parsed);
+    // console.log("message.timestamp is", message_parsed.timestamp);
     var item = {
         "os_id": {"S": Guid.raw()},
-        "timestamp": {"N": new Date(message.timestamp).valueOf().toString()},
-        "message": {"S": JSON.stringify(message)}
+        "timestamp": {"N": new Date(message_parsed.timestamp).valueOf().toString()},
+        // "message": {"S": JSON.stringify(message)}
+        "message": {"S": message}
     };
     return item;
 };
@@ -31,37 +34,53 @@ var messageToItem = function(message) {
 var dynamoDbPutMessage = function(message) {
     var params = {
         "TableName": worker.config.SEGMENT_RAW_DATA_TABLE_NAME,
-        "Item": messageToItem(message)
+        "Item": message
     }
     dd.putItem(params, function(err, data) {
         if (data && Object.keys(data).length > 0) {
             console.log("got data back from putItem:", data);
         }
         if (err) {
-            console.log(err);
+            console.log("error from putItem:", err);
+            // console.log("message that errored:", message);
         }
         else {
-            console.log("no error. exiting.");
+            console.log("dynamodb putitem successful. message ID:", message.os_id.S);
             message.delete();
         }
     });
 };
 
 //debug
-if (worker.params) {
-    //console.log("type of params:", typeof worker.params);
-    dynamoDbPutMessage(worker.params);
-}
+// if (worker.params) {
+//     console.log("type of params:", typeof worker.params);
+//     dynamoDbPutMessage(worker.params);
+// }
+// console.log("typeof imq:", typeof imq);
+// console.log("imq:", imq);
+// imq.queues(options, function(error, body) {
+//     if (error) {
+//         console.log("error getting queues:", error);
+//     }
+//     if (body) {
+//         console.log("queues body:", body);
+//     }
+// });
 //end debug
 
-var options = {n: 100};
+// var options = {n: 100};
+var options = {n: 5};
 queue.get_n(options, function(error, messages) {
     if (error) {
         console.log("error getting messages from queue:", error);
     }
     if (messages) {
+        console.log("successfully got", messages.length.toString(), "messages");
         messages.forEach(function(message) {
-            dynamoDbPutMessage(message);
+           dynamoDbPutMessage(messageToItem(message.body));
         });
+        // console.log("type of messages:", typeof messages);
+        // console.log("type of messages[0]:", typeof messages[0]);
+        // console.log("and a message:", messages[0]);
     }
 });
